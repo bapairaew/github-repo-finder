266342c96d3329c09c159215d97429b8355b102c
narudom <bapairaew@hotmail.com>
2017-03-09 22:20:17 +0700

    switch to react-lite to reduce bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ const config = {
   entry: __dirname + '/app.js',
   resolve: {
     modules: ['src', 'node_modules'],
-    extensions: ['.js']
+    extensions: ['.js'],
+    alias: {
+      'react': 'react-lite',
+      'react-dom': 'react-lite'
+    }
   },
   output: {
     path: 'dist/',
@@ -17,7 +21,34 @@ const config = {
       include: __dirname,
       loader: 'babel-loader'
     }]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      comments: false,
+      compress: {
+        unused: true,
+        dead_code: true,
+        warnings: false,
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        drop_console: true,
+        sequences: true,
+        booleans: true,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
 };
 
 module.exports = config;
